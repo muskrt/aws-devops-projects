c52e6f0dd2b8f665198a1f31d8d574f282dbcca4
muskrt <kurtmustafa5003@gmail.com>
2022-12-24 07:31:05 -0500

    test

--- shell-scripts/basic-commands/random_number.sh
+++ shell-scripts/basic-commands/random_number.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+echo -e "----script_content-----\n"
+cat << EOF
+#!/bin/bash
+RANDOM=2342342343434
+for i in \`seq 10\`
+do 
+  echo \$RANDOM
+done
+EOF
+
+
+echo -e "\n\n-----Results-----\n\n"
 RANDOM=2342342343434
 for i in `seq 10`
 do 
