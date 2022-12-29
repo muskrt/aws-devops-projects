e7963b0f66ed01ec492ea4e9782432b8356acea6
muskrt <kurtmustafa5003@gmail.com>
2022-12-29 06:50:13 -0500

    test

--- Devops/Docker/docker-01-welcome-to-docker/App/welcome.py
+++ Devops/Docker/docker-01-welcome-to-docker/App/welcome.py
@@ -5,12 +5,13 @@ app=Flask(__name__)
 
 @app.route('/')
 def main():
-    return "welcome to the test application of docker.<h3>Created By Mustafa kurt.</h3>"
-@app.route('/about'):
+    return f"welcome to the test application of docker.<h3>Created By Mustafa kurt.</h3>"
+
+@app.route('/about')
 def about_page():
     return "this is the about page of sample application.<h3>Created By Mustafa Kurt.</h3>"
 if __name__=="__main__":
-    app.run(debug=True,port=80)
+    app.run(host="0.0.0.0",port=80)
 
 
 
