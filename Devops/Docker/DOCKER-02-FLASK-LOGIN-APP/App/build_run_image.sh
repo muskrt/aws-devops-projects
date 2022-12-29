612d15bce7934f2486eaa441cacbb199b6e992e2
muskrt <kurtmustafa5003@gmail.com>
2022-12-29 09:45:01 -0500

    test

--- Devops/Docker/DOCKER-02-FLASK-LOGIN-APP/App/build_run_image.sh
+++ Devops/Docker/DOCKER-02-FLASK-LOGIN-APP/App/build_run_image.sh
@@ -1,11 +1,2 @@
-#!/bin/bash 
-yum update -y 
-yum install  subversion -y 
-amazon-linux-extras instal docker -y 
-systemctl start docker 
-systemctl enable docker 
-usermod -a -G docker ec2-user 
-newgrp docker 
-svn export http://github.com/....
-sleep 5 
-cd dffd 
+#!/bin/bash
+docker image build -t muskrt/LOGINAPP:1.0 
