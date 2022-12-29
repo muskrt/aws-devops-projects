e7843703ed98217d616f4eb1ac75cc1767a4d8bf
muskrt <kurtmustafa5003@gmail.com>
2022-12-29 07:34:37 -0500

    test

--- Devops/Docker/docker-01-welcome-to-docker/App/docker-build.sh
+++ Devops/Docker/docker-01-welcome-to-docker/App/docker-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 amazon-linux-extras install docker -y
-systemctl docker start 
+systemctl start docker  
 systemctl enable docker  
 usermod -a -G docker ec2-user 
 newgrp docker 
-docker image build -t muskrt/welcomeapp:1.0 .
+docker image build -t muskrt/welcomeapp:1.0 -f /home/ec2-user/docker-01-welcome-to-docker/App/
 docker container run --rm -dit --network host --name myapp muskrt/welcomeapp:1.0
