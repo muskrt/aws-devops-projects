e50e73474762bb7f2c676e183652dcf0ac5bd756
muskrt <kurtmustafa5003@gmail.com>
2022-12-29 06:21:50 -0500

    test

--- Devops/Docker/docker-01-welcome-to-docker/App/docker-build.sh
+++ Devops/Docker/docker-01-welcome-to-docker/App/docker-build.sh
@@ -4,6 +4,5 @@ systemctl docker start
 systemctl enable docker  
 usermod -a -G docker ec2-user 
 newgrp docker 
-cd App
 docker image build -t muskrt/welcomeapp:1.0 .
 docker container run --rm -dit --network host --name myapp muskrt/welcomeapp:1.0
