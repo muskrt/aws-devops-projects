bf2c34422d54353b2b814050f3574154caff29a0
muskrt <kurtmustafa5003@gmail.com>
2022-12-30 21:44:27 -0500

    test

--- Devops/Ansible/work-book-1/main.tf
+++ Devops/Ansible/work-book-1/main.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "myec2" {
-    key_name = linux
-    instance_type = t2.micro
+
     
   
 }
--- Devops/Ansible/work-book-1/providers.tf
+++ Devops/Ansible/work-book-1/providers.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
   region = us-east-1
   profile = default
 }
