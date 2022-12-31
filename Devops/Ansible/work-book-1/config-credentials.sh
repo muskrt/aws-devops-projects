ad543fafe915b6dcaa7b9fdba94ae161e1245be6
muskrt <kurtmustafa5003@gmail.com>
2022-12-30 21:42:17 -0500

    test

---
+++ Devops/Ansible/work-book-1/config-credentials.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "enter keyfile with absolute path: " KEY_LOCATION
+echo $KEY_LOCATION
+
---
+++ Devops/Ansible/work-book-1/main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "myec2" {
+    key_name = linux
+    instance_type = t2.micro
+    
+  
+}
