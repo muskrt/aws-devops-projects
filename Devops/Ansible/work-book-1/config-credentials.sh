39f405ab73816168f609dd094d84b0bed84f7344
muskrt <kurtmustafa5003@gmail.com>
2022-12-31 01:35:29 -0500

    test

--- Devops/Ansible/work-book-1/config-credentials.sh
+++ Devops/Ansible/work-book-1/config-credentials.sh
@@ -3,3 +3,5 @@
 read -p "enter keyfile with absolute path: " KEY_LOCATION
 echo $KEY_LOCATION
 
+
+
--- Devops/Ansible/work-book-1/main.tf
+++ Devops/Ansible/work-book-1/main.tf
@@ -1,4 +1,10 @@
+
+
 resource "aws_instance" "myec2" {
+    ami = AMIID
+    instance_type= INSTANCETYPE
+    key_name=KEYNAME
+    security_groups = []
 
     
   
