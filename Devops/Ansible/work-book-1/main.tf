b5271ce3c718b5f6a7300d988430983cde4c5eb8
muskrt <kurtmustafa5003@gmail.com>
2023-01-02 15:28:57 -0500

    test

--- Devops/Ansible/work-book-1/main.tf
+++ Devops/Ansible/work-book-1/main.tf
@@ -1,4 +1,18 @@
 
+data "aws_ami" "AMAZONLINUX2"{
+    most_recent = true 
+    owners = ["amazon"]
+    filter{
+        name= "virtualization-type"
+        values=["hvm"]
+
+    }
+    filter {
+      name = "name"
+      values = ["amzn2-ami-kernel-5.10*"]
+    }
+}
+
 
 resource "aws_instance" "myec2" {
     ami = AMIID
