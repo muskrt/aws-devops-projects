ab1ee817ee6ed347272b908f4ab5c5118037345a
muskrt <kurtmustafa5003@gmail.com>
2023-01-08 14:16:55 +0300

    TEST

--- Devops/Kubernetes/FLASK-REDIS-MONGODB/app.py
+++ Devops/Kubernetes/FLASK-REDIS-MONGODB/app.py
@@ -1,4 +1,8 @@
-from flask import Flask , render_template,request,jsonify
+from flask import Flask , render_template,request ,jsonify, Response 
+import json 
+import json2table
+from json2html import *
+from bson.objectid import ObjectId
 import redis
 from pymongo import MongoClient 
 import os 
@@ -7,9 +11,29 @@ import time
 
 app = Flask(__name__)
 redisdb=redis.Redis(host='redis',port=6379)
-mongodb=MongoClient('mongodb', 27017).flask_db
-todos=mongodb.flask_db.todos
-mongodb.flask_db.todos.insert_one({'abcd':'abcd'})
+mongodb=MongoClient('mongodb', 27017)
+db=mongodb.flask_db
+def mongo_read():
+    data=list(db.posts.find())
+    print(data)
+    for post in data:
+        # post["_id"]=str(post["_id"])
+        del post["_id"]
+    return json.dumps(data)
+def mongo_insert():
+    post={"tag":"mustafa","comment":"kurt"}
+    dbresponse=db.posts.insert_one(post)
+    post={"tag":"mustafa","comment":"kurt"}
+    dbresponse=db.posts.insert_one(post)
+
+    return Response(
+        response=json.dumps({"message":"user created"
+        ,"id":f"{dbresponse.inserted_id}"}),
+        status=200,
+        mimetype="application/json"
+    )
+
+
 def db_init():
     try:
         redisdb.set('foo','bar')
@@ -18,9 +42,14 @@ def db_init():
         print("cant connect")
 def db_search(name):
     return redisdb.get(name)
-@app.route('/mongo')
+
+@app.route('/mongoPost',methods=["GET","POST"])
 def mongo_page():
-    return jsonify(todos.find_one('abcd'))
+    if request.method == "POST":
+        return mongo_insert()
+@app.route('/mongoGet')
+def mongo_get():
+    return json2html.convert(json=mongo_read())
 @app.route('/',methods=['GET','POST'])
 def main_page():
     if request.method == "POST":
