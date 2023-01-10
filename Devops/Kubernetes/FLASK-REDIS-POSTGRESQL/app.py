53c21883cc9a246ba5118dbedfafbcb163f49a6a
muskrt <kurtmustafa5003@gmail.com>
2023-01-10 23:12:46 +0300

    test

--- Devops/Kubernetes/FLASK-REDIS-POSTGRESQL/app.py
+++ Devops/Kubernetes/FLASK-REDIS-POSTGRESQL/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def init_db(): pass 
+def db_get(): pass 
+def db_insert(): pass 
+def db_delete(): pass 
+def db_update(): pass 
 def login(): pass 
 def update(): pass 
 def delete(): pass 
