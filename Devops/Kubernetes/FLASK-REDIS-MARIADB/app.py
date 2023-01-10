52fa68ed7541c431b8a5d61137b355a6d38bd2be
muskrt <kurtmustafa5003@gmail.com>
2023-01-10 23:14:04 +0300

    test

--- Devops/Kubernetes/FLASK-REDIS-MARIADB/app.py
+++ Devops/Kubernetes/FLASK-REDIS-MARIADB/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def db_init():pass 
+def db_delete(): pass 
+def db_update(): pass 
+def db_get(): pass 
+def db_insert(): pass 
+
+def login(): pass 
 def update(): pass 
 def delete(): pass 
 def post(): pass 
