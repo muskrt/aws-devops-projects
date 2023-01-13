f637a66b71330e7570fac688bb4051a3ee7b79c9
muskrt <kurtmustafa5003@gmail.com>
2023-01-13 16:11:35 +0300

    test

--- Devops/Docker/DOCKER-03-FLASK-MYSQL-DOCKER_HUB/APP/app.py
+++ Devops/Docker/DOCKER-03-FLASK-MYSQL-DOCKER_HUB/APP/app.py
@@ -2,18 +2,26 @@ from flask import Flask
 
 app = Flask( __name__)
 
-def db_init(): pass 
+def db_init():
+    app.config['MYSQL_DATABASE_HOST'] = 'database'
+    app.config['MYSQL_DATABASE_USER'] = 'mustafa'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'toor1'
+    app.config['MYSQL_DATABASE_DB'] = 'bookstore_db'
+    app.config['MYSQL_DATABASE_PORT'] = 3306
+    global mysql 
+    mysql = MySQL()
+    mysql.init_app(app)
+    global connection 
+    connection = mysql.connect()
+    connection.autocommit(True)
+    global cursor 
+    cursor = connection.cursor()
+
 def db_get(): pass 
 def db_post(): pass 
 
 def login(): pass 
-def 
-
-@app.route('/')
-def main_page():
-    return "test"
-
-
+def main_page(): pass 
 
 
 if __name__=="__main__":
