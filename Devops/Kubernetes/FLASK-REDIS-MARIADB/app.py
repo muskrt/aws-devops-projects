bba45e8deb6b8fcb37f35d8cb3c75976e3db5830
muskrt <kurtmustafa5003@gmail.com>
2023-01-15 11:52:52 +0300

    test

--- Devops/Kubernetes/FLASK-REDIS-MARIADB/app.py
+++ Devops/Kubernetes/FLASK-REDIS-MARIADB/app.py
@@ -1,19 +1,32 @@
-from flask import Flask 
+from flask import Flask , render_template, url_for, request
 
 app = Flask(__name__)
 
-
 def db_init():pass 
-def db_delete(): pass 
-def db_update(): pass 
-def db_get(): pass 
-def db_insert(): pass 
+def db_delete(sourcedb=''): pass 
+def db_update(sourcedb=''): pass 
+def db_get(sourcedb=''): pass 
+def db_insert(sourcedb=''): pass 
 
-def login(): pass 
-def update(): pass 
-def delete(): pass 
-def post(): pass 
-def get(): pass 
+@app.route('/update')
+def update(): 
+    pass 
+@app.route('/delete')
+def delete():
+     pass 
+
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method =='GET':
+        return render_template('login.html')
+    elif request.method == "POST"
+        USERNAME=request.params['username']
+        PASSWORD=request.params['password']
+        if USERNAME == '' and PASSWORD == '':
+            return render_template('secure_page.html')
+        else:
+            return render_template('error.html')
+
 
 
 
--- Devops/Kubernetes/FLASK-REDIS-POSTGRESQL/app.py
+++ Devops/Kubernetes/FLASK-REDIS-POSTGRESQL/app.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 def init_db(): pass 
-def db_get(): pass 
-def db_insert(): pass 
-def db_delete(): pass 
-def db_update(): pass 
-def login(): pass 
+def db_get(sourcedb=''): pass 
+def db_insert(sourcedb=''): pass 
+def db_delete(sourcedb=''): pass 
+def db_update(sourcedb=''): pass 
+
 def update(): pass 
 def delete(): pass 
 def post(): pass 
