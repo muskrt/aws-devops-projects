448bb3aff7e6aaad1a0b701116a5350f975233b4
muskrt <kurtmustafa5003@gmail.com>
2023-01-19 21:18:35 +0300

    TEST

--- c++-projects/crow-simple-api/main.cpp
+++ c++-projects/crow-simple-api/main.cpp
@@ -7,9 +7,13 @@
 
 int main(){
 
+   crow::SimpleApp app; 
+
+    
+    CROW_ROUTE(app, "/")([](){
+        return "<h1>This Application Created By Mustafa Kurt.</h1>";
+    });
 
 
-
-
-    return 0;
+    app.port(5000).multithreaded().run();
 }
