9e122fd202f6d78cd6fc73cf2a9dbf02f7c63ba2
muskrt <kurtmustafa5003@gmail.com>
2023-01-19 21:22:42 +0300

    TEST

--- c++-projects/crow-simple-api/main.cpp
+++ c++-projects/crow-simple-api/main.cpp
@@ -10,7 +10,20 @@ int main(){
    crow::SimpleApp app; 
 
     
-    CROW_ROUTE(app, "/")([](){
+        CROW_ROUTE(app, "/").methods("GET"_method, "POST"_method)
+        ([](){
+        return "<h1>This Application Created By Mustafa Kurt.</h1>";
+    });
+        CROW_ROUTE(app, "/").methods("GET"_method, "POST"_method)
+        ([](){
+        return "<h1>This Application Created By Mustafa Kurt.</h1>";
+    });
+        CROW_ROUTE(app, "/").methods("GET"_method, "POST"_method)
+        ([](){
+        return "<h1>This Application Created By Mustafa Kurt.</h1>";
+    });
+        CROW_ROUTE(app, "/").methods("GET"_method, "POST"_method)
+        ([](){
         return "<h1>This Application Created By Mustafa Kurt.</h1>";
     });
 
