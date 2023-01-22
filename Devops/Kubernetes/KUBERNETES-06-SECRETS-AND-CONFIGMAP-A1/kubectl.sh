ef3aef888ef304cf5655e034a448980f5ab7272a
muskrt <kurtmustafa5003@gmail.com>
2023-01-22 21:38:48 +0300

    test

---
+++ Devops/Kubernetes/KUBERNETES-06-SECRETS-AND-CONFIGMAP-A1/kubectl.sh
@@ -0,0 +1,6 @@
+#1/bin/bash
+echo -n 'admin' > ./user_name.txt
+echo -n '1f2d1e2e67df' > ./pass_word.txt
+kubectl create secret generic db-user-pass --from-file=./user_name.txt --from-file=./pass_word.txt
+kubectl create secret generic db-user-pass-key --from-file=username=./username.txt --from-file=password=./password.txt
+kubectl describe secrets/db-user-pass
