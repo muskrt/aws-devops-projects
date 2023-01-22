d97322b80012aa916b700e25c24f6bf7a4e6b9d3
muskrt <kurtmustafa5003@gmail.com>
2023-01-22 21:54:43 +0300

    test

--- Devops/Kubernetes/KUBERNETES-06-SECRETS-AND-CONFIGMAP-A1/kubectl.sh
+++ Devops/Kubernetes/KUBERNETES-06-SECRETS-AND-CONFIGMAP-A1/kubectl.sh
@@ -4,3 +4,4 @@ echo -n '1f2d1e2e67df' > ./pass_word.txt
 kubectl create secret generic db-user-pass --from-file=./user_name.txt --from-file=./pass_word.txt
 kubectl create secret generic db-user-pass-key --from-file=username=./username.txt --from-file=password=./password.txt
 kubectl describe secrets/db-user-pass
+k exec -it pod -- bash
