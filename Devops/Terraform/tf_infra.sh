66a0ed2a6b1f0a18cfdd20ee6c92795a6838eaf5
muskrt <kurtmustafa5003@gmail.com>
2023-01-28 04:36:43 +0300

    test

--- Devops/Terraform/tf_infra.sh
+++ Devops/Terraform/tf_infra.sh
@@ -1,1 +1,12 @@
 #!/bin/bash 
+file_to_create_workspace=''
+files_to_create=[main.tf,providers.tf,outputs.tf,mustafa.auto.tfvars,variable.tf]
+
+IFS="="
+read -ra ADDR<<<"$name" 
+for i in "${ADDR[@]}";
+do
+echo "$i"
+done
+
+
