7d974a2d09e3104fa52ea9c40ec720dcdba7ff98
muskrt <kurtmustafa5003@gmail.com>
2023-01-28 05:13:11 +0300

    test

--- Devops/Terraform/tf_infra.sh
+++ Devops/Terraform/tf_infra.sh
@@ -3,12 +3,14 @@ file_to_create_workspace=''
 files_to_create=[main.tf,providers.tf,outputs.tf,mustafa.auto.tfvars,variable.tf]
 for var in "$@";
 do  
-echo $var
+
 if [[ ${#var} -gt 2 ]]
     then 
-    exit
+        echo TEST 
+    else 
+        echo $var 
     fi 
-    
+
 
 done
 
