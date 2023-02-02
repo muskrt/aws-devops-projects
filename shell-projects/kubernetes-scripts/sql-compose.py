adfc4b1da851e781f25f93d47220d00dda7aa7b7
muskrt <kurtmustafa5003@gmail.com>
2023-02-02 22:12:30 +0300

    test

---
+++ shell-projects/kubernetes-scripts/sql-compose.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+import os 
+import sys 
+import yaml
+from pprint import pprint
+
+def main():
+    file=open('docker-compose.yml','r')
+    test_dict=yaml.load(file, Loader=yaml.FullLoader)
+    # pprint(test_dict['services'])
+    for i in test_dict['services']:
+        print(i)
+
+
+
+if __name__=="__main__":
+    main()
