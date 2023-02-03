9a7c12797f3f80b6df85899be0f7d19f290dc300
muskrt <kurtmustafa5003@gmail.com>
2023-02-03 08:50:08 +0300

    test

--- shell-projects/kubernetes-scripts/sql-compose.py
+++ shell-projects/kubernetes-scripts/sql-compose.py
@@ -3,8 +3,30 @@ import os
 import sys 
 import subprocess
 import yaml
+
+
+
 from pprint import pprint
 import base64
+
+def code_base64(data,code_type):
+    if code_type == 'encode': 
+        data_bytes = data.encode("ascii")
+        base64_bytes = base64.b64encode(data_bytes)
+        base64_string = base64_bytes.decode("ascii")
+        return base64_string
+    else:
+        base64_bytes = data.encode("ascii")
+        data_bytes = base64.b64decode(base64_bytes)
+        decodedstring = data_bytes.decode("ascii")
+        return decodedstring
+
+def str_presenter(dumper, data):
+  if len(data.splitlines()) > 1:  # check for multiline string
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+  return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+yaml.add_representer(str, str_presenter)
+yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
 
 def prepare_templates():
     k8_file_names=['deployment','service','secret','configmap','hpa']
@@ -19,11 +41,58 @@ def prepare_templates():
 def create_service():
     pass 
 def create_secret(vars):
+    # print(vars)
+    for i in vars:
+        vars[i]=code_base64(vars[i],'encode')
     print(vars)
     
+    secret=k8_files['secret']
+    secret['data']=vars
+    print(secret)
+
+    
 def create_configmap():
-    files = str(subprocess.check_output("ls *.sql", shell=True).decode())
-    print('------',files)
+    SQL_FILE = str(subprocess.check_output("ls *.sql", shell=True).decode())
+    configmap=k8_files['configmap']
+    global sql_configmap_name
+    sql_configmap_name=SQL_FILE.replace('.','-')
+    sql_configmap_name=sql_configmap_name.strip('\n')
+    FILE = open(SQL_FILE.strip('\n'),'r')
+    sqldata=FILE.read()
+    configmap=eval(str(configmap).replace('KEY',SQL_FILE.strip('\n')))
+    configmap=eval(str(configmap).replace('CM',sql_configmap_name))
+    configmap['data'][SQL_FILE.strip('\n')]=sqldata
+    f=open(sql_configmap_name+'-configmap.yaml','w')
+    yaml.dump(configmap, f, sort_keys=False, default_flow_style=False)
+    f.close()
+    FILE.close()
+    
+    CNF_FILE = str(subprocess.check_output("ls *.cnf", shell=True).decode())
+    configmap=k8_files['configmap']
+    global cnf_configmap_name
+    cnf_configmap_name=CNF_FILE.replace('.','-')
+    cnf_configmap_name=cnf_configmap_name.strip('\n')
+    FILE = open(CNF_FILE.strip('\n'),'r')
+    cnfdata=FILE.read()
+    configmap=eval(str(configmap).replace('KEY',CNF_FILE.strip('\n')))
+    configmap=eval(str(configmap).replace('CM',cnf_configmap_name))
+    configmap['data'][CNF_FILE.strip('\n')]=cnfdata
+    f=open(cnf_configmap_name+'-configmap.yaml','w')
+    yaml.dump(configmap, f, sort_keys=False, default_flow_style=False)
+    f.close()
+    FILE.close()
+
+
+
+    # CNF_FILE = str(subprocess.check_output("ls *.cnf", shell=True).decode())
+
+
+    # sqldata = sqldata.encode('unicode_escape').decode()
+
+
+    # print(sqldata)
+    
+
 def create_deployment(compose,i):
     deployment=k8_files['deployment']
     deployment['spec']['template']['spec']['containers'][0]['image']=compose['services'][i]['image']
@@ -31,8 +100,7 @@ def create_deployment(compose,i):
     
     if 'environment' in compose['services'][i]:
         create_secret(compose['services'][i]['environment'])
-    if '-cs' in sys.argv:
-        create_configmap()
+
     # pprint(yaml.dump(deployment))
     # print(compose['services'][i])
 
@@ -42,6 +110,8 @@ def main():
     file=open('docker-compose.yml','r')
     test_dict=yaml.load(file, Loader=yaml.FullLoader)
     deployment=k8_files['deployment']
+    if '-cs' in sys.argv:
+        create_configmap()
     for i in test_dict['services']:
         create_deployment(test_dict,i)
         # deployment['spec']['template']['spec']['containers'][0]['image']=test_dict['services'][i]['image']
