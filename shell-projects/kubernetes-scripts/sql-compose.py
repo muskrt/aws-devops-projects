fb56b8b6d37570582d4be3f83a4c1a468c40b36c
muskrt <kurtmustafa5003@gmail.com>
2023-02-03 03:53:19 +0300

    test

--- shell-projects/kubernetes-scripts/sql-compose.py
+++ shell-projects/kubernetes-scripts/sql-compose.py
@@ -12,16 +12,20 @@ def prepare_templates():
     src_paths=['k8-config-files/deployment.yaml','k8-config-files/service.yaml','k8-config-files/secret.yaml','k8-config-files/configmap.yaml','k8-config-files/hpa.yaml']
     for name in k8_file_names:
         file=open(src_paths[ k8_file_names.index(name)])
-        k8_files[name]=file.read()
+        k8_files[name]=yaml.load(file, Loader=yaml.FullLoader)
         file.close()
-    return k8_files
 def main():
     file=open('docker-compose.yml','r')
     test_dict=yaml.load(file, Loader=yaml.FullLoader)
-    for i in test_dict['services']:
-        pprint(test_dict['services'][i])
-        break
-    pprint(prepare_templates()['deployment'])
+    # for i in test_dict['services']:
+    #     pprint(test_dict['services'][i])
+    #     break
+    deployment=k8_files['deployment']
+    pprint(deployment['spec']['template']['spec']['containers'][0]['env'])
+    exit()
+    file=open('testdeployment.yaml','w')
+    file.write(deployment)
+    file.close()
 
 
     
@@ -31,4 +35,5 @@ def main():
 
 
 if __name__=="__main__":
+    prepare_templates()
     main()
