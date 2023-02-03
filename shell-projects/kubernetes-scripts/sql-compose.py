ac7e29b6d055697450e123222b8f8135d6937933
muskrt <kurtmustafa5003@gmail.com>
2023-02-03 03:13:56 +0300

    test

--- shell-projects/kubernetes-scripts/sql-compose.py
+++ shell-projects/kubernetes-scripts/sql-compose.py
@@ -10,7 +10,15 @@ def main():
     # pprint(test_dict['services'])
     for i in test_dict['services']:
         print(i)
-    
+    k8_file_names=['deployment','service','secret','configmap','hpa']
+    k8_files={'deployment':{},'service':{},'secret':{},'configmap':{},'hpa':{}}
+    for i in k8_file_names:
+        print(k8_files[i])
+    src_paths=['k8-config-files/deployment.yaml','k8-config-files/service.yaml','k8-config-files/secret.yaml','k8-config-files/configmap.yaml','k8-config-files/hpa.yaml']
+    exit()
+
+
+
 
 
 if __name__=="__main__":
