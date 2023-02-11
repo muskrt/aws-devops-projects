39336e49484ff57d5350f2fa6913b1a6b6be8517
muskrt <kurtmustafa5003@gmail.com>
2023-02-11 13:19:24 +0300

    test

--- Devops/PROJECTS/DOCKER-BOOKSTORE_API_APP/INFRASTRUCTURE/Terraform/user-data.sh
+++ Devops/PROJECTS/DOCKER-BOOKSTORE_API_APP/INFRASTRUCTURE/Terraform/user-data.sh
@@ -8,10 +8,12 @@ newgrp docker
 curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 cd /home/ec2-user/
-until ls App
+until ls App 2>/dev/null 
 do 
     echo "waiting for files..."
 done 
+cd App/ 
+
 
 # while [  true ]; do  docker-compose up 2>/dev/null && break || continue;  done
 until docker-compose up 2>/dev/null
