5254292e025279eb2ea6117e0d4f9c0fe235644a
muskrt <kurtmustafa5003@gmail.com>
2023-02-12 18:02:10 +0300

    test

---
+++ Devops/Ansible/project-1/terraform/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>4"
+    }
+
+  }
+}
+
+provider "aws" {
+
+  region = "us-east-1"
+  profile = "default"
+}
+
+data "aws_ami" "AMIID" {
+    owners = ["amazon"]
+    most_recent = true 
+    filter {
+      name="name"
+      values=["amzn2-ami-hvm*"]
+    }
+  
+}
+
+resource "aws_instance" "ANSIBLESERVER" {
+  key_name = "linux"
+  instance_type = "t2.micro"
+  ami = data.aws_ami.AMIID.id  
+  count = 2
+
+  tags = {
+    name="worker-node-${count.index}"
+  }
+    provisioner "local-exec" {
+    command="echo worker-node-${count.index} ${self.public_ip} >> inventory.txt"
+  
+  }
+
+}
+
+output "ANSIBLESERVER" {
+    value = [aws_instance.ANSIBLESERVER[*].tags, aws_instance.ANSIBLESERVER[*].public_ip ]
+}
+
