b31aeac35138809671f43d2ff9fb9be241f22d23
muskrt <kurtmustafa5003@gmail.com>
2023-02-16 11:59:56 +0300

    test

--- Devops/Ansible/project-2/terraform/main.tf
+++ Devops/Ansible/project-2/terraform/main.tf
@@ -41,6 +41,12 @@ resource "aws_security_group" "ANSIBLESERVERSECGROUP" {
     to_port=80
     cidr_blocks=["0.0.0.0/0"]
   }
+    ingress{
+        from_port = 3306
+    protocol="tcp"
+    to_port=3306
+    cidr_blocks=["0.0.0.0/0"]
+  }
   egress{
     from_port = 0
     protocol="-1"
