51471311fc2a6716e7387229c8b95552b4ecb636
muskrt <kurtmustafa5003@gmail.com>
2023-02-28 17:54:19 +0300

    ttest

--- Devops/#####PROJECTS#####/JENKINS/terraform/main.tf
+++ Devops/#####PROJECTS#####/JENKINS/terraform/main.tf
@@ -13,105 +13,105 @@ provider "aws" {
   region = "us-east-1"
   profile = "default"
 }
-data "aws_ami" "REDHAT" {
-  owners = ["309956199498"] 
-  most_recent = true 
-  filter {
-    name="name"
-    values=["RHEL-9.0.0_HVM-20230127-x86_64*"]
-  }
+# data "aws_ami" "REDHAT" {
+#   owners = ["309956199498"] 
+#   most_recent = true 
+#   filter {
+#     name="name"
+#     values=["RHEL-9.0.0_HVM-20230127-x86_64*"]
+#   }
   
-}
-data "aws_ami" "AMAZON-LINUX" {
-    # owners = ["309956199498"] red hat 
-    owners=["amazon"]
-    most_recent = true 
-    # filter {
-    #   name="name"
-    #   values=["RHEL-9.0.0_HVM-20230127-x86_64*"]
-    # }
-    filter {
-      name="name"
-      values=["amzn2-ami-hvm*"]
-    }
+# }
+# data "aws_ami" "AMAZON-LINUX" {
+#     # owners = ["309956199498"] red hat 
+#     owners=["amazon"]
+#     most_recent = true 
+#     # filter {
+#     #   name="name"
+#     #   values=["RHEL-9.0.0_HVM-20230127-x86_64*"]
+#     # }
+#     filter {
+#       name="name"
+#       values=["amzn2-ami-hvm*"]
+#     }
   
-}
-variable "intance_tags" {
-    default = ["SLAVE_NODE1","SLAVE_NODE2"]
+# }
+# variable "intance_tags" {
+#     default = ["SLAVE_NODE1","SLAVE_NODE2"]
   
-}
+# }
 
-resource "aws_security_group" "ANSIBLESERVERSECGROUP" {
-  ingress {
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    protocol    = "tcp"
-    to_port     = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# resource "aws_security_group" "ANSIBLESERVERSECGROUP" {
+#   ingress {
+#     from_port   = 22
+#     protocol    = "tcp"
+#     to_port     = 22
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 8080
+#     protocol    = "tcp"
+#     to_port     = 8080
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
 
 
-  egress {
-    from_port   = 0
-    protocol    = -1
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     protocol    = -1
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   
-}
-resource "aws_instance" "JENKINS_SERVER" {
-  key_name = "linux"
-  instance_type = "t2.micro"
-  ami = data.aws_ami.AMAZON-LINUX.id
-  user_data=file("jenkins.sh")  
-  root_block_device {
-    volume_size = 12
-    }
-  vpc_security_group_ids = [aws_security_group.ANSIBLESERVERSECGROUP.id]
-  count = 1
+# }
+# resource "aws_instance" "JENKINS_SERVER" {
+#   key_name = "linux"
+#   instance_type = "t2.micro"
+#   ami = data.aws_ami.AMAZON-LINUX.id
+#   user_data=file("jenkins.sh")  
+#   root_block_device {
+#     volume_size = 12
+#     }
+#   vpc_security_group_ids = [aws_security_group.ANSIBLESERVERSECGROUP.id]
+#   count = 1
 
-  tags = {
-    Name="JENKINS_SERVER"
-  }
-    provisioner "local-exec" {
-    command=join("",["ansible-pam ${var.build_number} --dyninv ${self.tags.Name} ${self.public_ip} ",
-      "${var.inventory_path}/ ec2-user",])
+#   tags = {
+#     Name="JENKINS_SERVER"
+#   }
+#     provisioner "local-exec" {
+#     command=join("",["ansible-pam ${var.build_number} --dyninv ${self.tags.Name} ${self.public_ip} ",
+#       "${var.inventory_path}/ ec2-user",])
   
 
-  }
+#   }
 
 
-}
+# }
    
 
-resource "aws_instance" "SLAVE_NODE" {
-  key_name = "linux"
-  instance_type = "t2.micro"
-  ami = data.aws_ami.REDHAT.id 
-  count=3
-  root_block_device {
-    volume_size = 6
-    }
-  vpc_security_group_ids = [aws_security_group.ANSIBLESERVERSECGROUP.id]
+# resource "aws_instance" "SLAVE_NODE" {
+#   key_name = "linux"
+#   instance_type = "t2.micro"
+#   ami = data.aws_ami.REDHAT.id 
+#   count=3
+#   root_block_device {
+#     volume_size = 6
+#     }
+#   vpc_security_group_ids = [aws_security_group.ANSIBLESERVERSECGROUP.id]
 
-  tags = {
-    Name="${var.intance_tags[count.index]}"
-  }
-    provisioner "local-exec" {
-    command=join("",["ansible-pam ${var.build_number} --dyninv ${self.tags.Name} ${self.public_ip} ",
-      "${var.inventory_path}/ ec2-user",])
+#   tags = {
+#     Name="${var.intance_tags[count.index]}"
+#   }
+#     provisioner "local-exec" {
+#     command=join("",["ansible-pam ${var.build_number} --dyninv ${self.tags.Name} ${self.public_ip} ",
+#       "${var.inventory_path}/ ec2-user",])
 
   
-  }
+#   }
 
 
-}
+# }
    
    
 
--- Devops/#####PROJECTS#####/JENKINS/terraform/variables.tf
+++ Devops/#####PROJECTS#####/JENKINS/terraform/variables.tf
@@ -2,3 +2,23 @@ variable "inventory_path"{
     
 }
 variable "build_number"{}
+
+variable "mykey" {
+  default = "linux"
+}
+variable "myami" {
+  default = "ami-026b57f3c383c2eec"
+}
+variable "instancetype" {
+  default = "t2.micro"
+}
+variable "tag" {
+  default = "Jenkins_Server"
+}
+variable "jenkins-sg" {
+  default = "jenkins-server-sec-gr-208"
+}
+
+variable "user" {
+  default = "mustafa"
+}
